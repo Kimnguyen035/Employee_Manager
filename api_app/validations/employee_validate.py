6e2102aab889da64aae65aca2352dbd894ea5931
kimnt <you@example.com>
2022-12-19 17:34:40 +0700

    check number phone

--- api_app/validations/employee_validate.py
+++ api_app/validations/employee_validate.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from ..models.employee import Employee
 from configs.variable_response import *
+from configs.variable_listphone import *
 
 class IdGetEmployeeValidate(serializers.Serializer):
     id = serializers.IntegerField()
@@ -16,14 +17,7 @@ class ListTelephoneValidate(serializers.Serializer):
     phone_number = serializers.ListField()
     
     def validate_phone_number(self, value):
-        list_phone = [
-            '032','033','034','035','036','037','038','039',
-            '070','076','077','078','079',
-            '081','082','083','084','085',
-            '056','058',
-            '059'
-        ]
         for item in value:
-            if item[:3] not in list_phone or len(item) != 10:
+            if item[:3] not in LIST_PHONE_VN or len(item) != 10:
                 raise serializers.ValidationError(ERROR['phone_failed'])
         return value
