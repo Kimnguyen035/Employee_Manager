c5193f2becd277397c9dec81f585c849f758fc14
kimnt <you@example.com>
2022-12-19 11:41:32 +0700

    format urls

--- api_app/urls.py
+++ api_app/urls.py
@@ -2,23 +2,33 @@ from django.urls import path
 from .views.employee_view import *
 from .views.telephone_view import *
 
-urlpatterns = [
-    # employee
-    path('get-all', EmployeeView.as_view({'get':'get_all'})),
-    path('get-detail/<int:id>', EmployeeView.as_view({'get':'get_detail'})),
-    path('get-trash', EmployeeView.as_view({'get':'get_trash'})),
-    
-    path('create-emloyee', EmployeeView.as_view({'post':'create'})),
-    
-    path('edit-emloyee/<int:id>', EmployeeView.as_view({'put':'edit'})),
-    
-    path('delete-emloyee/<int:id>', EmployeeView.as_view({'delete':'delete'})),
-    path('restore-emloyee/<int:id>', EmployeeView.as_view({'delete':'restore'})),
-    path('drop-emloyee/<int:id>', EmployeeView.as_view({'delete':'drop'})),
-    
-    # telephone
-    path('get-all-phone', TelephoneView.as_view({'get':'all_phone'})),
-    path('detail-phone/<int:pk>', TelephoneView.as_view({'get':'all_phone'})),
-    path('create-phone', TelephoneView.as_view({'post':'create_phone'})),
-    path('edit-phone/<int:id>', TelephoneView.as_view({'put':'edit_phone'})),
-]
+url_items = {
+    'url_employee': [
+        path('get-all', EmployeeView.as_view({'get':'get_all'})),
+        path('get-detail/<int:id>', EmployeeView.as_view({'get':'get_detail'})),
+        path('get-trash-employee', EmployeeView.as_view({'get':'get_trash'})),
+        
+        path('create-emloyee', EmployeeView.as_view({'post':'create'})),
+        
+        path('edit-emloyee/<int:id>', EmployeeView.as_view({'put':'edit'})),
+        
+        path('delete-emloyee/<int:id>', EmployeeView.as_view({'delete':'delete'})),
+        path('restore-emloyee/<int:id>', EmployeeView.as_view({'delete':'restore'})),
+        path('drop-emloyee/<int:id>', EmployeeView.as_view({'delete':'drop'})),
+    ],
+    'url_phone': [
+        path('get-all-phone', TelephoneView.as_view({'get':'all_phone'})),
+        path('detail-phone/<int:id>', TelephoneView.as_view({'get':'detail_phone'})),
+        path('get-trash-phone', TelephoneView.as_view({'get':'get_trash'})),
+        path('create-phone', TelephoneView.as_view({'post':'create_phone'})),
+        path('edit-phone/<int:id>', TelephoneView.as_view({'put':'edit_phone'})),
+        path('delete-phone/<int:id>', TelephoneView.as_view({'delete':'delete_phone'})),
+        path('restore-phone/<int:id>', TelephoneView.as_view({'delete':'restore'})),
+        path('drop-phone/<int:id>', TelephoneView.as_view({'delete':'drop_phone'})),
+    ]
+}
+
+urlpatterns = []
+
+for item in url_items:
+    urlpatterns += url_items[item]
